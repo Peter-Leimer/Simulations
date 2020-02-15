157c82e0f5183da4bbaff6e10320c657a9f8c48d
Peter-Leimer <peterleimer6@gmail.com>
2020-02-15 11:15:32 -0600

    Part B

--- Git6.py
+++ Git6.py
@@ -2,16 +2,35 @@
 
 import random
 
-numFlips = int(input("How many Flips"))
+numFlips = int(input("How many Flips "))
 
-flips = [0]*2
-
+flips = [0]*numFlips
 i=0
+HRepeats = 0
+heads = 0
+tails = 0
 
 while(i<numFlips):
-    flip = random.randint(1,2)
-    flips[flip-1]+=1
+    flip = random.randint(0,1)
+    flips[i]+=flip
     i+=1
-
+    print (flip)
 for i in range(len(flips)):
-    print(f"{i+1}: {flips[i]}/{numFlips} = {flips[i]/numFlips*100}%")
+    if(flips[i] == 0):
+        tails+=1
+    elif(flips[i] == 1):
+        heads+=1
+   
+
+print(f"{heads}: {heads}/{numFlips} = {heads/numFlips*100}%")
+print(f"{tails}: {tails}/{numFlips} = {tails/numFlips*100}%")
+
+#Head repeats
+    
+for i in range(len(flips)):
+
+    if(flips[i:i+3] == [0,0,1]):
+        HRepeats+=1
+        
+
+print(f" Heads  repeats  only twice {HRepeats} times") 
