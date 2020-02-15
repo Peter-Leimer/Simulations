851fbe9ff2af6062ec9734430995493e815b7071
Peter-Leimer <peterleimer6@gmail.com>
2020-02-15 12:46:29 -0600

    A

--- Git6.py
+++ Git6.py
@@ -6,15 +6,17 @@ numFlips = int(input("How many Flips "))
 
 flips = [0]*numFlips
 i=0
-HRepeats = 0
+#HRepeats = 0
 heads = 0
 tails = 0
+runs = 0
 
 while(i<numFlips):
     flip = random.randint(0,1)
     flips[i]+=flip
     i+=1
-    print (flip)
+    
+    
 for i in range(len(flips)):
     if(flips[i] == 0):
         tails+=1
@@ -27,10 +29,26 @@ print(f"{tails}: {tails}/{numFlips} = {tails/numFlips*100}%")
 
 #Head repeats
     
-for i in range(len(flips)):
+#for i in range(len(flips)):
 
-    if(flips[i:i+3] == [0,0,1]):
-        HRepeats+=1
+ #   if(flips[i:i+3] == [0,0,1]):
+ #       HRepeats+=1
         
+#print(f" Heads  repeats  only twice {HRepeats} times") 
 
-print(f" Heads  repeats  only twice {HRepeats} times") 
+
+# user prompted search
+nope =0
+lookFor = int(input("Heads Or Tails(1-tails, 0-heads)? "))
+if(lookFor == 1):
+    nope = 0
+elif(lookFor == 0):
+    nope = 1
+run = int(input("How long is the run you're looking for? "))
+for i in range(len(flips)):
+    if(flips[i:i+run+1] == [lookFor]*run+[nope]):
+        runs+=1
+
+print(flips[0:numFlips])
+print(f" Your search happens {runs} times")
+
